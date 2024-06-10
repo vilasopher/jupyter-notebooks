878ee98b94ddcdafdf836a665e9619ba250e425b
Vilas Winstein <vilas.win@gmail.com>
2024-06-09 19:49:23 -0700

    pretty speeches

--- python/XY/autonomous_circular.py
+++ python/XY/autonomous_circular.py
@@ -11,7 +11,7 @@ import colorcet as cc
 N = 512
 F = 3000
 I = 500
-L = 508
+L = 1631
 S = 0.05
 
 
@@ -19,14 +19,17 @@ S = 0.05
 
 theta = None
 
-def InsideCircleSlow(i,j):
+def InsideInnerCircle(i,j):
     return (i+0.5-0.5*N)**2 + (j+0.5-0.5*N)**2 <= (0.5*N - 8)**2
 
-InsideCircleFast = np.array([[InsideCircleSlow(i,j) for i in range(N)] for j in range(N)])
+def OutsideOuterCircle(i,j):
+    return (i+0.5-0.5*N)**2 + (j+0.5-0.5*N)**2 > (0.5*N)**2
 
 def CircleBC(i,j):
-    if InsideCircleFast[i,j]:
+    if InsideInnerCircle(i,j):
         return theta[i,j]
+    elif OutsideOuterCircle(i,j):
+        return -10
     else:
         return np.arctan2(i+0.5-0.5*N, j+0.5-0.5*N)
 
@@ -43,6 +46,10 @@ def MetropolisUpdate(i,j,beta=1.1343):
 
 beta = lambda x : 1.1343 + 150 * (1+50*x**8) * (x-1/3)**5
 
+InsideCircleFast = np.array([[InsideInnerCircle(i,j) for i in range(N)] for j in range(N)])
+
+cmap=cc.m_cyclic_rygcbmr_50_90_c64
+cmap.set_under(color='black')
 
 # Run simulation (slow)
 
@@ -50,9 +57,10 @@ if L > -1:
     theta = np.load(f'circledata/{L:06d}.npy').reshape((N,N))
 else:
     theta = ra.uniform(-np.pi,np.pi,size=(N,N))
-    for i in range(N):
-        for j in range(N):
-            theta[i,j] = CircleBC(i,j)
+
+for i in range(N):
+    for j in range(N):
+        theta[i,j] = CircleBC(i,j)
 
 for frame in tqdm(range(L+1, F)):
     b = beta(frame/F)
@@ -66,4 +74,4 @@ for frame in tqdm(range(L+1, F)):
                     MetropolisUpdate(i,j,beta=b)
 
     np.save(f'circledata/{frame:06d}.npy',theta)
-    plt.imsave(f'circleimages/{frame:06d}.png', theta, format='png', cmap=cc.m_cyclic_rygcbmr_50_90_c64, vmin=-np.pi, vmax=np.pi)
+    plt.imsave(f'circleimages/{frame:06d}.png', theta, format='png', cmap=cmap, vmin=-np.pi, vmax=np.pi)
